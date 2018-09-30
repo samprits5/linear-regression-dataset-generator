e99ed6f22142b58f6aa27a3e9bf5b859baaae0b2
samprits5 <samprits5@gmail.com>
2018-10-01 00:25:10 +0530

    First commit

---
+++ data/dataset_gen.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 19:04:36 2018
+
+@author: sampr
+"""
+
+import os
+import random
+import csv
+
+
+def main():
+    
+     print("\n:: Linear Regression Dataset Generator ::\n\n")
+    
+     n = int(input("How many points : "))
+     
+     x = str(input("\nEnter X column name : "))
+     y = str(input("\nEnter Y column name : "))
+     
+     coeff = random.randrange(-10, 10)
+
+     intercept = random.randrange(-100, 100)
+     
+     path = "C:\\Users\\sampr\\Desktop"
+     
+     myfile = "dataset_sam.csv"
+     infilename = os.path.join(path,myfile)
+
+     
+     with open(infilename, 'w+') as csvfile:
+         spamwriter = csv.writer(csvfile)
+        
+
+         for i in range(0,n+1):
+         
+             if (i == 0): 
+                 list = [x , y]
+                 spamwriter.writerow(list)
+                 continue
+             x = random.randrange(-100, 100)
+    
+             y = coeff*x + intercept
+    
+             shuffle = random.randrange(-200, 200)
+    
+             y += shuffle
+             
+             list = [x , y]
+         
+             spamwriter.writerow(list)
+         
+     print("\nFile created - dataset_sam.csv")
+     
+
+
+if __name__== "__main__":
+  main()
